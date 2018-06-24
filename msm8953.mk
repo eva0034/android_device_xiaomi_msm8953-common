bd29cb38a82bc309a56dcb6716cc61818031f406
eva0034 <evans.john.96@outlook.com>
2018-06-24 20:13:00 +1000

    Use GoogleCamera Instead Of Stock
    Use Google Camera Instead Of Stock

--- msm8953.mk
+++ msm8953.mk
@@ -119,7 +119,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    SnapdragonCamera \
+    Gcamera \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
